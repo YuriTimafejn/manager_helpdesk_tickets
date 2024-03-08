f6135f2054438adc8dad83f86d5ae9f5fe9877a0
Yuri Timafejn <timafejn@gmail.com>
2024-03-08 14:08:51 -0300

    view: created layout to the home dashboard

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -7,7 +7,10 @@
             <div class="col">
                 <div class="mb-2 mt-1">
                     <h4 class="mb-2">Abertos</h4>
-                    <div class="table-responsive-sm border border-2 border-primary rounded-2">
+                    <div
+                        class="table-responsive-sm border border-2 border-primary rounded-2"
+                        style="height: 400px;"
+                    >
                         <table class="table table-sm table-hover align-middle">
                             <thead>
                             <tr>
@@ -47,7 +50,10 @@
 
                 <div class="mb-2 mt-1">
                     <h4 class="mb-2">Concluidos</h4>
-                    <div class="table-responsive-sm border border-2 border-success rounded-2">
+                    <div
+                        class="table-responsive-sm border border-2 border-success rounded-2"
+                        style="height: 250px;"
+                    >
                         <table class="table table-sm table-striped table-hover align-middle">
                             <thead>
                             <tr>
@@ -146,4 +152,145 @@
                 </div>
         </div>
     </div>
+
+{{--    New ticket div  --}}
+    <div
+        class="new-ticket container bg-dark-subtle p-2 border border-2 border-dark"
+        style="display: none; width: 925px; position: fixed; top: 50%; left:50%; transform: translate(-50%, -50%);"
+    >
+
+        <div class="row mb-1">
+            <div class="col-md-4">
+                <label for="" class="form-label">Data de abertura</label>
+                <input type="date" name="" id="" class="form-control">
+            </div>
+            <div class="col-md-3">
+                <label for="" class="form-label">&nbsp;</label>
+                <select name="" id="" class="form-select">
+                    <option>Prioridade</option>
+                    <option value=""></option>
+                </select>
+            </div>
+            <div class="col-md-3">
+                <label for="" class="form-label">&nbsp;</label>
+                <select name="" id="" class="form-select">
+                    <option>Solicitante</option>
+                    <option value=""></option>
+                </select>
+            </div>
+            <div class="col-md-2" style="margin: auto 0 0 0;">
+                <input type="button" value="Novo Solicitante" class="btn btn-primary" style="height: 36px">
+            </div>
+        </div>
+
+        <div class="row mb-1">
+            <div class="col-md-12">
+                <label for="" class="form-label">Chamado</label>
+                <input type="text" name="" id="" class="form-control">
+            </div>
+        </div>
+
+        <div class="mb-3 mt-1">
+            <label for="" class="form-label">Descrição</label>
+            <textarea class="form-control" id="" rows="3"></textarea>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12 d-flex justify-content-end">
+                <input type="button" value="Cancelar" class="btn btn-danger me-1">
+                <input type="button" value="Abrir Solicitação" class="btn btn-primary me-1">
+            </div>
+        </div>
+
+    </div>
+
+{{--    New requirer div       --}}
+    <div
+        class="new-submitter container bg-dark-subtle p-2 border border-2 border-dark"
+        style="display: none; width: 825px; position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%);"
+    >
+
+        <div class="row m-2">
+            <div class="col-md-12">
+                <label for="" class="form-label">Nome</label>
+                <input type="text" class="form-control">
+            </div>
+
+        </div>
+
+        <div class="row m-2">
+            <div class="col-md-6">
+                <label for="" class="form-label">&nbsp;</label>
+                <select name="" id="" class="form-select">
+                    <option>Empresa</option>
+                    <option value=""></option>
+                </select>
+            </div>
+            <div class="col-md-6">
+                <label for="" class="form-label">E-mail contato</label>
+                <input type="email" name="" id="" class="form-control">
+            </div>
+        </div>
+
+        <div class="row m-1">
+            <div class="col-md-12 d-flex justify-content-end">
+                <input type="button" value="Cancelar" class="btn btn-danger me-1">
+                <input type="button" value="Adicionar solicitante" class="btn btn-primary me-1">
+            </div>
+        </div>
+
+
+    </div>
+
+{{--    Report div        --}}
+    <div
+        class="generate-report container-md bg-success-subtle p-2 border border-2 border-dark rounded"
+        style="display: none;width: 800px; position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%);"
+    >
+        <div class="d-flex justify-content-center">
+            <h3>Exportar relatórios de Atividades</h3>
+        </div>
+
+        <div class="row">
+            <div class="col-md-4">
+                <label for="" class="form-label">&nbsp;</label>
+                <button class="btn btn-outline-primary">Relatório mês: SETEMBRO</button>
+            </div>
+            <div class="col-md-4">
+                <label for="" class="form-label">&nbsp;</label>
+                <button class="btn btn-outline-primary">Relatório último semestre</button>
+            </div>
+            <div class="col-md-4">
+                <label for="" class="form-label">&nbsp;</label>
+                <button class="btn btn-outline-primary">Relatório ano: 2023</button>
+            </div>
+        </div>
+        <div class="row mt-2">
+            <div class="d-flex justify-content-center"><h5>Busca por intervalo</h5></div>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="input-group">
+                        <span class="input-group-text">Data inicial</span>
+                        <input type="date" name="" id="" class="form-control">
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="input-group">
+                        <span class="input-group-text">Data final</span>
+                        <input type="date" name="" id="" class="form-control">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row mt-4 mb-2">
+            <div class="d-flex justify-content-md-evenly">
+                <input type="button" value="Cancelar" class="btn btn-danger me-1">
+                <input type="button" value="Gerar PDF" class="btn btn-warning me-1">
+                <input type="button" value="Enviar por e-mail" class="btn btn-light me-1">
+            </div>
+        </div>
+
+    </div>
+
 @endsection
